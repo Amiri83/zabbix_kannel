0da6c9c345e9abb71ddaaded7197ec53e7486b67
Amir Amiri <Amiri83@gmail.com>
2022-08-01 10:08:16 +0430

    Update send_sms.sh

--- send_sms.sh
+++ send_sms.sh
@@ -2,5 +2,5 @@
 #SMS with Kannel
 
 message=$(echo $2|sed -e 's/ /%20/g') 
-curl --location --request GET  "http://Kannel_ip:13013/cgi-bin/sendsms?username=simple&password=simple1&from=smsc_Account_name&to=$1&text=$message"
+curl --location --request GET  "http://Kannel_ip:13013/cgi-bin/sendsms?username=Kannle_send_sms_suer&password=kannel_send_smsmpass&from=smsc_Account_name&to=$1&text=$message"
 
